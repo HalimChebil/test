240f1585ff258e5a989dc8c31a86f26670ade3de
HalimChebil <63865650+HalimChebil@users.noreply.github.com>
2024-10-18 18:29:05 +0200

    Create app.ts

---
+++ app.ts
@@ -0,0 +1,7 @@
+// app.ts
+window.onload = () => {
+    const headerText = document.getElementById("header-text");
+    if (headerText) {
+        alert("Hallo! Welcome to the website.");
+    }
+};
